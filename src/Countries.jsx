4aaa202ab4e97efba941187f966198b2ce418b9d
Durga Sandeep Vasarla <Vdurgasandeep@gmail.com>
2024-02-27 19:27:50 +0530

    first

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import axios from 'axios'
-import { Link, Outlet } from 'react-router-dom'
+import React from 'react';
+import axios from 'axios';
+import { Link, Outlet } from 'react-router-dom';
+
 function Countries() {
-    var [countries, setcountries] = React.useState([])
-    React.useEffect(()=> {
-        axios.get("https://restcountries.com/v3.1/all").then((res)=> {
-            setcountries(res.data)
-        })
-    },[])
-    console.log(countries)
-  return (
-    <div>
-        <ul>
-            {
-                countries.map((country)=> {
-                    return <li><Link to={`details/${country.name.common}`}>{country.name.common}</Link></li>
-                })
-            }
-        </ul>
-        <div>
-            <Outlet></Outlet>
+    var [countries, setcountries] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all").then((res) => {
+            setcountries(res.data);
+        });
+    }, []);
+
+    console.log(countries);
+
+    return (
+        <div className='d-flex w-75'>
+            <ul>
+                {countries.map((country) => (
+                    <li key={country.name.common}>
+                        <Link to={`details/${country.name.common}`}>
+                            {country.name.common}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <div className='w-25'>
+                <Outlet />
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Countries
+export default Countries;
